f3570028455f87538231e06ae7f1755a1e453b99
Kiran N <n07kiran@gmail.com>
2024-08-06 12:54:22 +0530

    Added approximation to tsp

--- travellingSalesPerson.cpp
+++ travellingSalesPerson.cpp
@@ -3,52 +3,14 @@
 
 using namespace std;
 
-
-void rPermutation(vector<int> &nums,int start,int end,vector<vector<int>> &ans)
-{
-	if(start > end)
-	{
-		if(nums[0] == 0)
-		{
-			nums.push_back(0);
-			ans.push_back(nums);
-			//for(auto &x : nums)
-                        //{
-                                //cout<<x<<" ";
-                        //}
-                        //cout<<"\n";
-			nums.pop_back();
-		}
-	}
-	else
-	{
-		for(int i=start;i<=end;i++)
-        	{
-                	swap(nums[start],nums[i]);
-                	rPermutation(nums,start+1,end,ans);
-			swap(nums[start],nums[i]);
-        	}
-	}
-}
+vector<vector<int>> getGraph(int &n);
+void rPermutation(vector<int> &nums,int start,int end,vector<vector<int>> &ans);
+int nearestNeighbourApprox(vector<vector<int>> G,int n,int source=0);
 
 int main()
 {
 	int noVertices;
-	cout<<"Enter the number of Vertices : ";
-	cin>>noVertices;
-
-	vector<vector<int>> CostMat (noVertices, vector<int> (noVertices,999));
-
-	cout<<"Enter the Cost Matrix (999 for self loop and no edge): \n";
-
-	for(int i=0;i<noVertices;i++)
-	{
-		for(int j=0;j<noVertices;j++)
-		{
-			cin>>CostMat[i][j];
-		}
-	}
-
+	vector<vector<int>> CostMat = getGraph(noVertices);
 
 	vector<int> V (noVertices,0);
 	for(int i=0;i<noVertices;i++)
@@ -56,6 +18,7 @@ int main()
 		V[i] = i;
 	}
 
+	// Storing all possible Permutatoins
 	vector<vector<int>> ans;
 	rPermutation(V,0,noVertices-1,ans);
 	
@@ -63,7 +26,7 @@ int main()
 	int noOfPermutations = ans.size();
 	vector<int> pathCost (noOfPermutations,-1);
 
-	int minCost = 999;
+	int optimalCost = 999;
 	int minPathIndex = -1;
 	for(int i=0;i<ans.size();i++)
 	{
@@ -95,10 +58,10 @@ int main()
 			pathCost[i] = -1;
 		}
 
-		if(cost < minCost)
+		if(cost < optimalCost)
 		{
 			minPathIndex = i;
-			minCost = cost;
+			optimalCost = cost;
 		}
 	}
 
@@ -108,14 +71,15 @@ int main()
 		return -1;
 	}
 
+	cout<<"All Possible Paths : \n";
 	for(int i=0;i<noOfPermutations;i++)
 	{
 		if(pathCost[i] != -1)
 		{
 			for(auto &x : ans[i])
-        		{
-                		cout<<x<<" --> ";
-        		}
+			{
+				cout<<x<<" --> ";
+			}
 			cout<<" = "<<pathCost[i]<<"\n";
 		}
 	}
@@ -125,6 +89,111 @@ int main()
 	{
 		cout<<x<<" --> ";
 	}
-	cout<<" = "<<minCost<<"\n";
+	cout<<" = "<<optimalCost<<"\n";
 	cout<<"\n";
+
+	int approxCost = nearestNeighbourApprox(CostMat,noVertices);
+
+	if(approxCost == -1){
+		cout<<"Can not calculate approximation with nearestNeighbour approach from this source \n";
+		return -1;
+	}
+
+	float err =  ( (float) (approxCost - optimalCost) / optimalCost ) * 100;
+
+	cout<<"Aprroximation Cost using nearest NeighBour = "<<approxCost<<"\n";
+	cout<<"Error in approximation = "<<err<<" % \n";
+}
+
+void rPermutation(vector<int> &nums,int start,int end,vector<vector<int>> &ans)
+{
+	if(start > end)
+	{
+		if(nums[0] == 0)
+		{
+			nums.push_back(0);
+			ans.push_back(nums);
+			//for(auto &x : nums)
+                        //{
+                                //cout<<x<<" ";
+                        //}
+                        //cout<<"\n";
+			nums.pop_back();
+		}
+	}
+	else
+	{
+		for(int i=start;i<=end;i++)
+        	{
+                	swap(nums[start],nums[i]);
+                	rPermutation(nums,start+1,end,ans);
+			swap(nums[start],nums[i]);
+        	}
+	}
+}
+
+vector<vector<int>> getGraph(int &n)
+{
+        cout<<"Enter the number of vertices : ";
+        cin>>n;
+
+        vector<vector<int>> G (n,vector<int> (n,0));
+
+        cout<<"Enter the Adjacency COST Matrix of G ( 0 or no edge and self loop ): \n";
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>G[i][j];
+				if(i==j || G[i][j] == 0){
+					G[i][j] = 999;
+				}
+            }
+        }
+        return G;
+}
+
+// Nearest Neighbour Approximation;
+int nearestNeighbourApprox(vector<vector<int>> G,int n,int source){
+
+	vector<int> visited (n,0);
+
+	bool pathExist = true;
+	int pathCost = 0;
+	visited[source] = 1;
+
+	int u = source , v;
+	for(int noEdge=1;noEdge<=n-1;noEdge++){
+		v = -1;
+		int minEdge = 999;
+
+		//Find the minimum of unVisited edges:
+		for(int i=0;i<n;i++){
+			if(!visited[i] && G[u][i] < minEdge ){
+				// cout<<u<<","<<i<<" = "<<G[u][i]<<"\n";
+				v = i;
+				minEdge = G[u][i];
+			}
+		}
+
+		if(v == -1){
+			bool pathExist = false;
+			break;
+		}
+
+		pathCost += G[u][v];
+		visited[v] = 1;
+		// cout<<u<<","<<v<<" = "<<pathCost<<"\n";
+		u = v;
+	}
+
+	// CHecking if the last vertex has edge back to source vertex;
+	if(G[u][source] != 999){
+		pathCost += G[u][source];
+	}else{
+		pathExist = false;
+	}
+
+	if(pathExist) return pathCost;
+	return -1;
 }
